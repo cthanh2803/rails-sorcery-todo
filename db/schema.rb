8236e60ffbbcc3c1e03d4d64e956056b731cbf9a
n-thanh <thban2803@gmail.com>
2022-03-30 17:20:04 +0900

    add admin column to users table, add sample data

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_30_004604) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.integer "admin", default: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@example.com", admin: true, password: "123123", password_confirmation: "123123")
+User.create(email: "member@example.com", admin: false, password: "123123", password_confirmation: "123123")
+
+Post.create(title: "Title 1", body: "Body 1")
+
+Post.create(title: "Title 2", body: "Body 2")
+
+Article.create(title: "Title 1", tag: "Tag 1")
+
+Article.create(title: "Title 2", tag: "Tag 2")
