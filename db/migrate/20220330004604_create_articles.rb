df457453d8ed5eeac561bf78ec820c1d0cd4bf03
n-thanh <thban2803@gmail.com>
2022-03-30 09:50:22 +0900

    edit scaffold for Post, Article

--- db/migrate/20220330004604_create_articles.rb
+++ db/migrate/20220330004604_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title
       t.string :tag
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_002841) do
+ActiveRecord::Schema.define(version: 2022_03_30_004604) do
 
   create_table "articles", charset: "utf8mb3", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_03_30_002841) do
 
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
